3c95a2092d46ce1a4ea7a1baafd4b5387a24cacd
Thiago Meneses <thi.santosmeneses@gmail.com>
2022-06-06 01:12:39 -0300

    Original

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,39 @@ function clicou2() {
         document.querySelector('.slide').classList.add('clicou1');
     }
 }
+function scrollToggle() {
+    const element = document.querySelector('#scrollTopButton');
+    if (window.scrollY == 0) {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'flex';
+    }
+}
+
+window.addEventListener('scroll', scrollToggle);
+
+function backToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+function menuToggle() {
+    let menu = {
+        show: "calc(100vh - 100px)",
+        hidden: "0px"
+    }
+    let element = document.getElementById('menu-area');
+
+    if (element.style.height == menu.show) {
+        element.style.display = 'none';
+        element.style.height = menu.hidden;
+    } else {
+        element.style.display = 'flex';
+        element.style.height = menu.show;
+    };
+};
+
+
 
